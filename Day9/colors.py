7347c68855ce44b4648d441b5231c24a63c24c6e
Nitesh-mint <nitesh.raya058@gmail.com>
2022-08-11 10:59:11 +0545

    Basic color  values

---
+++ Day9/colors.py
@@ -0,0 +1,7 @@
+#pygame has some color variations so.
+#We use tuple for color definitions
+Red = (255,0,0)
+Green = (0,255,0)
+Blue = (0,0,255)
+Black = (0,0,0) #no red green or blue
+White = (255,255,255) #full red green and blue
