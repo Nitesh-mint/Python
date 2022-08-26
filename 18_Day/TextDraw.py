6514cb95f357015bca4c4987ee028a111d16ee05
Nitesh-mint <nitesh.raya058@gmail.com>
2022-08-26 07:58:04 +0545

    initial commit

---
+++ 18_Day/TextDraw.py
@@ -0,0 +1,28 @@
+import pygame
+from pygame.locals import *
+import sys
+from simpleText import *
+
+#defining constants
+Black  = (255,255,255)
+WHITE = (0,0,0)
+WINDOW_WIDTH = 500
+WINDOW_HEIGHT = 500
+frames = 30
+
+#initialze
+pygame.init()
+window = pygame.display.set_mode((WINDOW_WIDTH,WINDOW_HEIGHT))
+clock = pygame.time.Clock()
+
+#loop
+while True:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            sys.exit()
+    window.fill(Black)
+    oText = SimpleText(window,(150,50),"Nitesh",(WHITE))
+    oText.draw()
+    pygame.display.update()
+    clock.tick(frames)
