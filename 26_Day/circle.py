35c9eb5fe2683220f4753c2b505b164cb0db05a3
Nitesh-mint <nitesh.raya058@gmail.com>
2022-09-03 06:35:58 +0545

    initial commit

---
+++ 26_Day/circle.py
@@ -0,0 +1,37 @@
+import pygame
+import random
+import math
+
+#define colors
+RED = (255,0,0)
+GREEN = (0,255,0)
+BLUE = (0,0,255)
+
+class Circle():
+    def __init__(self,window,maxWidth,maxHeight):
+        self.window = window
+        self.color = random.choice(RED,GREEN,BLUE)
+        self.X = random.randrange(1,maxWidth-100)
+        self.Y = random.randrange(25,maxHeight-100)
+        self.radius = random.randrange(10,50)
+        self.centerX = self.X + self.radius
+        self.centerY = self.Y + self.radius
+        self.rect = pygame.Rect(self.x,self.Y,self.radius*2,self.raduis*2)
+        self.shape = "Circle"
+
+    def clickedInside(self,mousePoint):
+        distance = math.sqrt(((mousePoint[0] - self.centerX)**2)+((mousePoint[1]-self.centerY)**2))
+        if distance <= self.radius:
+            return True
+        else:
+            return False
+
+    def getArea(self):
+        theArea = math.pi*(self.radius ** 2)
+        return theArea
+    
+    def getType(self):
+        return self.Type
+
+    def draw(self):
+        pygame.draw.circle(self.window,self.color,(self.centerX,self.centerY),self.radius,0)
---
+++ 26_Day/square.py
@@ -0,0 +1,30 @@
+import pygame
+import random
+
+#set up colors
+RED = (255,0,0)
+GREEN = (0,255,0)
+BLUE = (0,0,255)
+
+class square():
+    def __init__(self,window,maxWidth,maxHeight):
+        self.window = window
+        self.WidthAndHeight = random.randrange(10,100)
+        self.color  = random.choice(RED,GREEN,BLUE)
+        self.x = random.randrange(1,maxWidth - 100)
+        self.y = random.randrange(1,maxHeight  - 100)
+        self.rect = pygame.Rect(self.x,self.y,self.WidthAndHeight,self.widthAndHeight)
+        self.shapeType = "square"
+    
+    def clikedInside(self,mousePoint):
+            clicked = self.rect.collidepoint(mousePoint)
+            return clicked
+        
+    def getType(self):
+        return self.shapeType
+    
+    def getArea(self):
+        theArea = self.WidthAndHeight * self.WidthAndHeight
+    
+    def draw(self):
+        pygame.draw.rect(self.window,self.color,self.x,self.y,(self.WidthAndHeight,self.WidthAndHeight))
